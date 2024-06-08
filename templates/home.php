8b07e9b842bedce82269184cc44707e9719fb04c
OluwaTobba <142856021+OluwaTobba@users.noreply.github.com>
2024-06-08 20:10:22 +0100

    Update home.php

--- templates/home.php
+++ templates/home.php
@@ -1,4 +1,4 @@
 <main>
     <h1>Welcome to MTC TEMPLATES</h1>
-    <p>Your Source for High-Quality Website Templates.</p>
+    <p>Your Source for High-Quality Website Templates.  Choose Your Choice, Checkout and It all Yours!!</p>
 </main>
